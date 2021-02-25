3ebd0b8e83f04d8ebc42802480314467194078d3
Vse Putyom <bertraid777@gmail.com>
2021-02-25 16:06:53 +0300

    19

--- js/script.js
+++ js/script.js
@@ -1,101 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    //! Урок номер 18
-    //! Скрипты и время выполнения setTimeout, setInterval и requestAnimationFrame Объект Date
+    //! Урок номер 19
+    //! Параметры  документа, окна и работы с ним
 
 
-    //* setTimeout принимает 2 аргумета, первый функция колбек и второй это время, через какой промежуток времени запустить эту функцию, пример:
+    //* чтобы получить ширину и высоту открытого окна в браузере, мы должны вывести их значения, например
+    // const hign = document.documentElement.clientHeight;
+    // console.log(hign);
+    // const width = document.documentElement.clientWidth;
+    // console.log(width);
 
-    // setTimeout(function(){
-    //     console.log('сообщение в консоль');
-    // }, 3000);
+    //* чтобы узнать на сколько пиксилей мы ушли от верхней точки
 
-    //* setInterval различия будут в том, что через какой интервал будет запускаться колбек функция, теперь каждые 2 сек будет запускаться
+    const scroll = document.documentElement.scrollTop;
+    console.log(scroll);
 
-    // setInterval(function(){
-    //     console.log('привет я set interval');
-    // }, 2000);
+    //* если нам нужны размеры с полосой прокрутки и бордорами , то нам нужно использовать offsetHeight и offsetWidth
+    const hign = document.documentElement.offsetHeight;
+    console.log(hign);
+    const width = document.documentElement.offsetWidth;
+    console.log(width);
 
-    // let count = 0;
-    // setInterval(function () {
-    //     count++;
-    //     console.log('привет я set interval ' + count);
-    // }, 2000);
+    //* если нам нужно получить ширену и высоту обьекта с учётом скроллов , то нам нужно свойство scrollHeight and  scrollWidth
 
-    //* есть функции которые останавливают их обеих к примеру clearInterval(); и clearTimout();
-    //* она принимает индитификатор setInterval, как его определить ?
-    //* нужно создать переменную let idInterval и вывести её в clearInterval
-    //* чтобы была в этом логика , мы setInterval поместим в setTimeout и поставим время
+    const hign = document.documentElement.scrollHeight;
+    console.log(hign);
+    const width = document.documentElement.scrollWidth;
+    console.log(width);
 
-    // let count = 0;
+    //* пример того , что прсле нажатия кнопки скрол сдвигается на какое-то колличество пиксилей, с помощью метода scrollBy(0, 10);
+    document.querySelector('.buttun').addEventListener('click', () => {
+        block.scrollBy(0, 10);
 
-    // let idInterval = setInterval(function () {
-    //     count++;
-    //     console.log('привет я set interval ' + count);
-    // }, 2000);
+        //? для перемещения по скролу единежды
+        block.scrollTo(10, 50);
+    })
 
-    // setTimeout(function () {
-    //     clearInterval(idInterval);
-    // }, 7000);
-
-
-    // let getMassage = function (name) {
-    //     console.log('привет ' + name + '!');
-    // };
-
-    // let count = 0;
-
-    // let idInterval = setInterval(getMassage, 2000, 'сука, я твоя смерть');
-
-    // setTimeout(getMassage, 5000, 'мудак');
-
-    //* ну ксажилению у этих способов есть минусы страница может быть не в активной вкладке, и анимация может продолжаться, также
-    //* анимация работает не ссинхронно с обоновлением экрана , что ведёт к торможению компа, для того чтобы это всё избежать
-    //* была создана фунция реквестАнимейшен фрейм,  которая помогая оптимизировать анимации, помогая делать их более плавными, также
-    //* использовать меньше ресурсво и связывать анимации в одно целое. 
-    //* для этого создаём за пределами функции переменную flyInterval, название производное
-    //* а в нутри функции пишем эту переменную flyInterval = requestAnimationFrame(сюда передадим имя функции), для её завершения
-    //* мы прописываем cancelAnimationFrame(flyInterval);
-
-    // count = 0;
-    // let flyInterval;
-    // let flyAnimate = function () {
-    //     flyInterval = requestAnimationFrame(flyAnimate);
-    //     count++;
-    //     if (count < 350) {
-    //         worm.style.top = count + 'px';
-    //         airplane.style.left = сount * 2 + 'px';
-    //     } else if (count < 500) {
-    //         airplane.style.left = сount * 2 + 'px';
-    //     } else {
-    //         cancelAnimationFrame(flyInterval);
-    //     }
-    //     console.log(count);
-    // };
-
-    // flyInterval = requestAnimationFrame(flyAnimate);
-
-    //* чтоыбы правильно работать с requestAnimationFrame нужно уметь работать с Date , знать текущее время, окончание работы анимации и т.д
-
-    //! Date 
-
-    //* используем get для получения какой - то даты часов, минут и т.д
-    //* и наоборт для того, чтобы установить используем setData, пример
-
-    let data = new Date();
-    data.setDate(data.getDate() + 100);
-    console.log('день недели ' + data.getDay());
-    //* мы также можем высчитать какое число будет через 100 дней
-    //* отчёт времени в js начался хранится в милисекундах , прошедших  с полуночи 1970 года 1 января это называется timeStend
-    //* чтобы его получить нужно воспользоваться методом getTime();
-    console.log(data.getTime());
-    //* также мы можем вписать в скобочки Data() числа наугад, и мы получим результат с 1970 года 1 января + это количество милисекунд, пример
-
-    let data2 = new Date(-615214413123);
-    console.log(data2);
-
-
-
-
+    //* чтобы узнать на сколько блок находится по координатам отокна браузера т.к в js все координаты вычесляются от верхнего левого угла
+    document.querySelector('.buttun').addEventListener('click', () => {
+        const domRect = block.getBoundingClientRect();
+        console.log(domRect.top);
+        console.log(domRect.left);
+    })
 });
